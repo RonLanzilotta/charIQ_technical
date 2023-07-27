508c9934ea06e790a480d7aeb26d0746b79d5258
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-07-27 09:43:15 -0400

    setting up my own API since alpha vantage keeps booting me

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // declare variables for the API call
 const API_KEY = 'K0HTOOWI401WQMWH';
 const SYMBOL = 'IBM';
+// change this to be set dynamically by the user
 const MOVING_AVERAGE_INTERVAL = 20;
-const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${SYMBOL}&apikey=${API_KEY}`;
+const API_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${SYMBOL}&apikey=${API_KEY}`;
 
 // select the chart element and its rendering context to enable drawn content
 const chart = document.getElementById("chart")
@@ -11,14 +12,64 @@ const ctx = chart.getContext("2d")
 // API call
 async function fetchStockData() {
     try {
-        // fetches API data dynamically
-        const response = await fetch(URL);
-        // converts to JSON after 
+        // fetches API data dynamically and returns a Promise.
+        // const response = await fetch(API_URL);
+        
+        // converts to JSON after the Promise is resolved.
         const data = await response.json();
+        // isolates the OHLCC data from the JSON with the 'Time Series (Daily)' key.
         const stockData = data['Time Series (Daily)']
 
-        function stockPrices([date, value]) {
-            
-        }
+        // Object.entries() returns an array of arrays which contain two elements each, derived from the stockData object's key value pairs.
+        // Here, .map() returns an array of objects with the date and the stock's closing price can be found by accessing line 4 (close) of the raw data.
+        const stockPrices = Object.entries(stockData).map(([date, values]) => {
+            return { date, price: parseFloat(values['4. close'])};
+        })
+
+        drawChart(stockPrices)
+
+    } catch (error) {
+        console.error('error fetching stock data:', error)
     }
 }
+
+function drawChart(data) {
+    // Create arrays to store the dates and prices in separate arrays for mapping and dom manipulation
+    const dates = data.map(entry => entry.date);
+    const prices = data.map(entry => entry.price);
+    console.log(prices)
+
+    // Calculate the moving average based on given interval
+    const movingAverages = [];
+    // start with 
+    for (let i = MOVING_AVERAGE_INTERVAL - 1; i < prices.length; i++) {
+        const sum = prices.slice(i - MOVING_AVERAGE_INTERVAL + 1, i + 1).reduce((acc, val) => acc + val, 0);
+        
+        movingAverages.push(sum / MOVING_AVERAGE_INTERVAL);
+    }
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, chart.width, chart.height);
+
+    // Draw the stock price line chart
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, chart.height - (prices[0] * chart.height / Math.max(...prices)));
+    for (let i = 1; i < prices.length; i++) {
+        ctx.lineTo(i * chart.width / (prices.length - 1), chart.height - (prices[i] * chart.height / Math.max(...prices)));
+    }
+    ctx.stroke();
+
+    // Draw the 20-day moving average line chart
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, chart.height - (movingAverages[0] * chart.height / Math.max(...prices)));
+    for (let i = 1; i < movingAverages.length; i++) {
+        ctx.lineTo(i * chart.width / (movingAverages.length - 1), chart.height - (movingAverages[i] * chart.height / Math.max(...prices)));
+    }
+    ctx.stroke();
+}
+
+fetchStockData()
