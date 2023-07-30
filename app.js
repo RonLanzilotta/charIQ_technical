dac3985d21ad7ee03628cb781e09a8efaefb4002
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-07-30 13:40:49 -0400

    creating a legend

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const API_URL = 'https://chartiq-api-8511c706644d.herokuapp.com/api/data'
+const API_URL = 'https://chartiq-api-8511c706644d.herokuapp.com/api/data';
+
 // Sets the Moving Average Interval to a default of 20, to be changed by the user based on input.
-let MOVING_AVERAGE_INTERVAL = document.getElementById("movingAvgInt").defaultValue = 20;
+let movingAverageInterval = document.getElementById("movAvgIntInput").defaultValue = 20;
 
-// let MOVING_AVERAGE_INTERVAL = 20;
-
-let stockPriceData
-
-// select the chart element and its rendering context to enable drawn content
-// const chart = document.getElementById("chart")
-// const ctx = chart.getContext("2d")
+let stockPriceData;
 
 // API call
 async function fetchStockData() {
@@ -19,56 +14,59 @@ async function fetchStockData() {
         // converts to JSON after the Promise is resolved.
         const data = await response.json();
         // isolates the OHLCC data from the JSON with the 'Time Series (Daily)' key.
-        const stockData = data[1]['Time Series (Daily)']
+        const stockData = data[1]['Time Series (Daily)'];
         // Object.entries() returns an array of arrays which contain two elements each, derived from the data object's key value pairs.
         // Here, .map() returns an array of objects with the date and the stock's closing price can be found by accessing 'Close' from the data object.
         const stockPrices = Object.entries(stockData).map(([date, values]) => {
-            return { date, price: parseFloat(values['Close'])};
+            return { date, price: parseFloat(values['Close']) };
         })
-        stockPriceData = stockPrices
+        stockPriceData = stockPrices;
 
-        drawChart(stockPrices)
+        drawChart(stockPrices);
 
     } catch (error) {
-        console.error('error fetching stock data:', error)
+        console.error('error fetching stock data:', error);
     }
 }
 
 function handleMovingAvgIntUpdate(stockPriceData) {
     const inputString = document.getElementById('movingAvgInt');
-    const inputNum = parseInt(inputString.value)
+    const inputNum = parseInt(inputString.value);
 
-    MOVING_AVERAGE_INTERVAL = inputNum;
-    drawChart(stockPriceData)
+    movingAverageInterval = inputNum;
+    drawChart(stockPriceData);
 }
 
 function drawChart(data) {
     // Ratio between the device's physical pixel resolution to the CSS pixel resolution
     const dpr = window.devicePixelRatio || 1;
-    const chart = document.getElementById("lineChart")
-    const ctx = chart.getContext("2d")
-    const grid = chart.getContext("2d")
+    const chart = document.getElementById("lineChart");
+    const ctx = chart.getContext("2d");
 
     // Increase the resolution of the chart
     const chartWidth = chart.clientWidth * dpr;
     const chartHeight = chart.clientHeight * dpr;
 
-    chart.width = chartWidth
-    chart.height = chartHeight
+    chart.width = chartWidth;
+    chart.height = chartHeight;
+    
     chart.style.width = `${chartWidth / dpr}px`;
     chart.style.height = `${chartHeight / dpr}px`;
-    
+
     ctx.scale(dpr, dpr);
     // Create separate arrays to store the dates and prices for mapping and dom manipulation
-    const dates = data.map(entry => entry.date);
+    // const dates = data.map(entry => entry.date);
     const prices = data.map(entry => entry.price);
+
+    // hard coded dates for x-axis labels
+    const months = ['', '08/22', '09/22', '10/22', '11/22', '12/22', '01/23', '02/23', '03/23', '04/23', '05/23', '06/23', '07/23', ''];
 
     // Makes the prices along the Y-axis dynamic according to the stock prices in our data.
     let yAxisUpperBound = 0;
     let yAxisLowerBound = Infinity;
     let yAxisArr = [];
 
-    console.log(prices)
+    console.log(prices);
 
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] > yAxisUpperBound) {
@@ -88,15 +86,15 @@ function drawChart(data) {
     }
 
     // yAxisArr.push(yAxisUpperBound + yAxisInterval)
-    yAxisArr.unshift(0)
+    yAxisArr.unshift('')
     console.log(yAxisArr)
 
     // Calculate the moving average based on given interval
     const movingAverages = [];
-    for (let i = MOVING_AVERAGE_INTERVAL - 1; i < prices.length; i++) {
-        const sum = prices.slice(i - MOVING_AVERAGE_INTERVAL + 1, i + 1).reduce((acc, val) => acc + val, 0);
-        
-        movingAverages.push(sum / MOVING_AVERAGE_INTERVAL);
+    for (let i = movingAverageInterval - 1; i < prices.length; i++) {
+        const sum = prices.slice(i - movingAverageInterval + 1, i + 1).reduce((acc, val) => acc + val, 0);
+
+        movingAverages.push(sum / movingAverageInterval);
     }
 
     // Clear the canvas
@@ -104,20 +102,13 @@ function drawChart(data) {
 
     // Create variables for cleaner formulas below. Both variables help to adjust the height at which the lines are drawn in the canvas.
     const maxPrice = Math.max(...prices)
-    const adjustHeight = maxPrice * -1.8
+    const adjustHeight = maxPrice * -1.55;
 
     // grid preference variables
     const gridQuadrantSize = 57.1;
 
     const xAxisDistanceGridLines = 6;
     const yAxisDistanceGridLines = 1;
-
-    const months = ['', '08/22', '09/22', '10/22', '11/22', '12/22', '01/23', '02/23', '03/23', '04/23', '05/23', '06/23', '07/23', '']
-
-    const yAxisPrices = ['', '$115', '$125', '$135', '$145', '$155', '', '']
-
-    // const xAxisStartingPoint = { number: 1, suffix: '' }
-    // const yAxisStartingPoint = { number: 1, suffix: '' }
 
     const linesX = Math.floor(chartHeight / gridQuadrantSize)
     const linesY = Math.floor(chartWidth / gridQuadrantSize)
@@ -127,15 +118,15 @@ function drawChart(data) {
         ctx.beginPath();
         ctx.lineWidth = 1;
 
-        if( i == xAxisDistanceGridLines) {
+        if (i == xAxisDistanceGridLines) {
             ctx.strokeStyle = "#000000";
             ctx.lineWidth = 2;
         } else {
             ctx.strokeStyle = "#c7c7c7";
         }
-    
-    // Not sure what this is for yet
-        if( i == linesX ) {
+
+        // Not sure what this is for yet
+        if (i == linesX) {
             ctx.moveTo(0, gridQuadrantSize * i);
             ctx.lineTo(chartWidth, gridQuadrantSize * i);
         }
@@ -152,15 +143,15 @@ function drawChart(data) {
         ctx.beginPath();
         ctx.lineWidth = 1;
 
-        if( i == yAxisDistanceGridLines) {
+        if (i == yAxisDistanceGridLines) {
             ctx.strokeStyle = "#000000";
             ctx.lineWidth = 2;
         } else {
             ctx.strokeStyle = "#c7c7c7";
         }
-    
-    // Not sure what this is for yet
-        if( i == linesY ) {
+
+        // Not sure what this is for yet
+        if (i == linesY) {
             ctx.moveTo(gridQuadrantSize * i, 0);
             ctx.lineTo(gridQuadrantSize * i, chartHeight);
         }
@@ -177,39 +168,41 @@ function drawChart(data) {
     // ctx.translate(gridQuadrantSize * yAxisDistanceGridLines, gridQuadrantSize * xAxisDistanceGridLines)
 
     // Ticks marks along the positive X-axis
-    for ( i = 0; i < linesY; i++) {
+    for (i = 0; i < linesY; i++) {
 
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
 
         // Draw a tick mark 6px long (-3 to 3)
-        ctx.moveTo(gridQuadrantSize * i + 0.5, -3);
-        ctx.lineTo(gridQuadrantSize * i + 0.5, 3);
+        ctx.moveTo(gridQuadrantSize * i + 0.5, -4);
+        ctx.lineTo(gridQuadrantSize * i + 0.5, 4);
         ctx.stroke();
 
         // Text value at that point
         ctx.font = '9px Arial';
         ctx.textAlign = 'center';
-        ctx.fillText(months[i], gridQuadrantSize * i - 2, 15);
+        ctx.fillText(months[i], gridQuadrantSize * i - 2, 14);
     }
 
     // Ticks marks along the positive Y-axis
-    for ( i = 0; i < linesX; i++) {
+    for (i = 0; i < linesX; i++) {
 
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
 
         // Draw a tick mark 6px long (-3 to 3)
-        ctx.moveTo(-3, -gridQuadrantSize * i + 0.5);
-        ctx.lineTo(3, -gridQuadrantSize * i + 0.5);
+        ctx.moveTo(-4, -gridQuadrantSize * i + 0.5);
+        ctx.lineTo(4, -gridQuadrantSize * i + 0.5);
         ctx.stroke();
 
         // Text value at that point
         ctx.font = '9px Arial';
         ctx.textAlign = 'center';
-        ctx.fillText(`$${yAxisArr[i]}`, -16, -gridQuadrantSize * i - 2);
+        yAxisArr[i] === '' ? 
+            ctx.fillText(yAxisArr[i], -16, -gridQuadrantSize * i - 2) :
+            ctx.fillText(`$${yAxisArr[i]}`, -16, -gridQuadrantSize * i - 2);
     }
 
     // Draw the stock price line chart
